5e60b6c6cacc3fe339b83ff0a6d8f03afcb95756
dilog777 <132608049+dilog777@users.noreply.github.com>
2023-10-28 17:13:27 +0300

    Added tensorflow

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 
-#include "lib.h"
+#include <filesystem>
 
-int main(int, char **)
+#include <boost/format.hpp>
+
+#include "tf_classifier.h"
+
+const char *const USAGE_MESSAGE = "Usage: fashio_mnist <DATA> <MODEL>";
+const char *const PATH_NOT_EXIST_MESSAGE = "Path '%s' not exist";
+
+const size_t IMAGE_WIDTH = 28;
+const size_t IMAGE_HEIGHT = 28;
+
+
+
+int main(int argc, char *argv[])
 {
-	std::cout << "build " << buildNumber() << std::endl;
+	if (argc != 3)
+	{
+		std::cout << USAGE_MESSAGE << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string dataPath = argv[1];
+	std::string modelPath = argv[2];
+
+	for (const auto &path : { dataPath, modelPath })
+	{
+		bool exist = std::filesystem::exists(path);
+		if (!exist)
+		{
+			std::cout << USAGE_MESSAGE << std::endl;
+			std::cout << (boost::format { PATH_NOT_EXIST_MESSAGE } % path) << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
+	auto clf = mnist::TfClassifier { modelPath, IMAGE_WIDTH, IMAGE_HEIGHT };
+
 	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	return EXIT_SUCCESS;
 }
--- src/library/lib.cpp
+++
@@ -1,8 +0,0 @@
-#include "lib.h"
-
-
-
-int buildNumber()
-{
-	return 0;
-}
